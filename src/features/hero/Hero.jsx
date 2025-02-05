e87c565c36ff4467b928d5c611ab82afbcb7e51b
Samuel Gonzalez <sigpay@outlook.com>
2025-02-05 20:47:01 -0300

    Hero/Banner component finished

--- src/features/hero/Hero.jsx
+++ src/features/hero/Hero.jsx
@@ -1,16 +1,19 @@
 import './Hero.css';
-import React from 'react';
-//import { Button } from '../../shared/components/Button';
-//import { LottieAnimation } from '../../shared/components/Lottie';
-
-//Animated Lottie
-//import animationPath from '../../assets/images/rocket.json'
 
 function Hero() {
-    return (
-        <React.Fragment>
-        </React.Fragment>
-    )
+  return (
+    <div className="hero-container">
+      <div className="hero-content">
+        <h1 className="hero-title">
+          <span className="hero-title--small">Transformamos tu Empresa implementando</span><br /><span className="brand-color">Inteligencia Artificial</span>
+        </h1>
+        <h2 className="hero-subtitle">
+          Llevamos tu empresa al siguiente nivel adaptandola al nuevo panorama del siglo XXI
+        </h2>
+        <button className="hero-button">Descubre Más</button>
+      </div>
+    </div>
+  );
 }
 
-export { Hero }
+export { Hero };
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 //Components
 import { Menu } from '../features/menu/Menu'
+import { Hero } from '../features/hero/Hero'
 
 function Landing() {
     return (
         <React.Fragment>
             <Menu />
+            <Hero />
         </React.Fragment>
     )
 }

a977f44e11bde4ccc373078853dbc39f3fee1fb6
Samuel Gonzalez <sigpay@outlook.com>
2025-02-13 09:27:19 -0300

    feat: Add basic structure for About component

--- src/features/About/About.jsx
+++ src/features/About/About.jsx
@@ -4,7 +4,17 @@ import './About.css';
 function About(){
     return (
         <section className="about-container">
+            <div className="about-title-container">
 
+            </div>
+            <div className="about-content">
+                <div className="about-text">
+
+                </div>
+                <div className="about-image">
+                    
+                </div>
+            </div>
         </section>
     )
 }

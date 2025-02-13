88ff8a6e20010c1cdb9601de4d2d1f049b01f399
Samuel Gonzalez <sigpay@outlook.com>
2025-02-12 21:46:18 -0300

    We import the Hero component.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//Components
+import { Hero } from '../features/Hero/Hero'
+
+function Home() {
+    return (
+        <>
+            <Hero />
+        </>
+    )
+}
+
+export { Home }

4c14710732e851bae33c15840872a702136c12fd
Samuel Gonzalez <sigpay@outlook.com>
2025-02-12 21:45:49 -0300

    We import the Home component and global styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './shared/styles/global.css'
+import './shared/styles/keyframes.css'
+
+//Pages
+import { Home } from './pages/Home'
 
 function App() {
 
   return (
     <>
-      
+      <Home />
     </>
   )
 }

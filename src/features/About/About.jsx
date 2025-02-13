0c6b2b04e0831f96296152830563555619472db7
Samuel Gonzalez <sigpay@outlook.com>
2025-02-13 09:23:41 -0300

    feat: Add initial About component structure

---
+++ src/features/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+function About(){
+    return (
+        <section className="about-container">
+
+        </section>
+    )
+}
+
+export { About }
